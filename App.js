dd66c8060a30974b7d2903420a283f41c1ce26a5
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-05-17 08:53:55 -0700

    Achieved bare minimum MVP: You can scan and paste from clipboard

--- App.js
+++ App.js
@@ -1,12 +1,59 @@
+import { useEffect, useState } from 'react';
+// import { Camera } from 'expo-camera';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import * as Clipboard from 'expo-clipboard';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const [camPermission, setCamPermssion] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [barcodeData, setBarcodeData] = useState(null);
+
+  const getCamPermission = async () => {
+    const { status } = await BarCodeScanner.requestPermissionsAsync();
+    setCamPermssion(status === 'granted');
+  };
+
+  const handleBarcodeScan = async ({ type, data }) => {
+    setScanned(true);
+    console.log(type, data);
+    console.log(typeof type, typeof data);
+    setBarcodeData({ type, data });
+    await Clipboard.setStringAsync(data);
+  };
+
+  const handleScanAgain = () => {
+    setScanned(false);
+    setBarcodeData(null);
+  };
+
+  useEffect(() => {
+    getCamPermission();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      {scanned ? (
+        <Button title="Press to scan again" onPress={handleScanAgain} />
+      ) : (
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarcodeScan}
+          style={StyleSheet.absoluteFillObject}
+        />
+      )}
+
+      {barcodeData ? (
+        <>
+          <Text>Barcode type: {barcodeData.type}</Text>
+          <Text>Barcode data: {barcodeData.data}</Text>
+          <Text>Barcode data loaded into clipboard</Text>
+        </>
+      ) : (
+        <Text>Scanning scanner</Text>
+      )}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
